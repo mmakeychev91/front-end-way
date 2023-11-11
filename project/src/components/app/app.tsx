7f52cbd1a86f2cd56ef6bef5f6d07860b96e8604
mmakeychev91 <mmakejcev@gmail.com>
2023-11-11 14:55:59 +0300

    feat: развернул пустой проект на react

---
+++ project/src/components/app/app.tsx
@@ -0,0 +1,15 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import React from 'react';
+
+type Props = {
+};
+
+const App = ({
+}: Props): JSX.Element => (
+  <BrowserRouter>
+    <Routes>
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
